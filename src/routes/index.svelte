9c43d221a804ccf354786c56e5d0a7d3e3b7475e
ElouanMre <elouan.marie.23@eigsi.fr>
2022-04-26 18:06:13 +0200

    1 file modified

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,5 +4,13 @@
 	<body>
 		<h1>MonPetitSaucisson.com</h1>
 		<p>De bonnes victuailles pour de bonnes personnes !</p>
+		<nav>
+			<ul>
+				<li>Saucisson</li>
+				<li>Pâtés</li>
+				<li>Vins</li>
+				<li>Bières</li>
+			</ul>
+		</nav>	
 	</body>
 	
