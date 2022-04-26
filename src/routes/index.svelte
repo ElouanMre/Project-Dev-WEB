d5409be09cc05f7277a88959776d8170cd5213b9
ElouanMre <elouan.marie.23@eigsi.fr>
2022-04-26 17:59:35 +0200

    3 files modified

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+	<svelte:head>
+			<title> MonPetitSaucisson.com - Boutique de saucisson</title>
+	</svelte:head>	
+	<body>
+		<h1>MonPetitSaucisson.com</h1>
+		<p>De bonnes victuailles pour de bonnes personnes !</p>
+	</body>
+	
